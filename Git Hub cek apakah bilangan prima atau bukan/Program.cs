4ef6aab39a996cd60a4ff805b99e1ba60ce31c1f
Andrew Lin <Andrew Lin@LAPTOP-B21CI6OA>
2021-12-03 10:54:15 +0700

    Add project files.

---
+++ Git Hub cek apakah bilangan prima atau bukan/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Git_Hub_cek_apakah_bilangan_prima_atau_bukan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //cek apakah bilangan prima atau bukan
+            Console.WriteLine("cek bilangan prima");
+            if (n == 0 || n == 1) ;
+            int main() {;
+                int bil("7");
+            else if ("") ;
+            bool angka_prima : true;
+            bool bukan_angka prima : false; 
+            Console.WriteLine("cek bilangan non prima");
+            if (n == 0 || n == 1) ;
+            int main()
+            {
+                ;
+                int bil("7");
+            else if ("");
+            bool angka_prima : true;
+            bool bukan_angka prima: false;
+            if ("args.bilangan non prima = false");
+        }
+    }
+}
